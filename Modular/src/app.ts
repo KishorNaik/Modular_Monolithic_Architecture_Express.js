39bdd501796ae8316ae0a9dd074cfd9fb1af68ec
Kishor Naik <kishor.naik011.net@gmail.com>
2024-04-12 19:07:33 +0530

    Add JSON Limit

--- Modular/src/app.ts
+++ Modular/src/app.ts
@@ -49,7 +49,7 @@ export class App {
         this.app.use(hpp());
         this.app.use(helmet());
         this.app.use(compression());
-        this.app.use(express.json());
+        this.app.use(express.json({ limit: '50mb' }));
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(cookieParser());
         this.app.use(actuator());
